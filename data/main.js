92c953c0b63480d7104da4f15c2018c6cca36b9a
petersembi <petersembi@gmail.com>
2023-02-06 23:38:42 +0300

    modified main.js

--- data/main.js
+++ data/main.js
@@ -44,8 +44,19 @@
    {
        var popup = '<img style="width: 100%" src=\'' + markers[i].img_url +   '\'> <p class="text-center"> ' + markers[i].contact_person +'<br> ' + markers[i].contact_description;
            console.log(popup);
+
+       var myIcon = L.icon({
+    iconUrl: 'https://gnrc.net/wp-content/uploads/2023/02/marker-icon.png',
+ 
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
        
-       var m = L.marker([markers[i].latitude, markers[i].longitude])
+       var m = L.marker([markers[i].latitude, markers[i].longitude], {icon: myIcon})
        m.addTo(map);
        m.bindPopup(popup);
        m.on('mouseover',  function(e) {
@@ -55,3 +66,8 @@
            this.closePopup();
        });
    }
+
+
+
+
+   
